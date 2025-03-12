7e48b4c1c1209ddcb83c598c8e5f3f35e344cea2
Gael Chauvet <gchauvet@z4r12p3.42lyon.fr>
2025-03-12 17:11:11 +0100

    two cmd can be lunche in pipex in two proc

--- pipex.c
+++ pipex.c
@@ -1,27 +1,39 @@
 #include "pipex.h"
+#include "libft/libft.h"
+#include <cstdlib>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-void	child_proc(int argc, char **argv, char **envp)
+void	child_proc(char **argv, char **envp)
 {
-
+	char	**cmd;
+	cmd = ft_split(argv[2], ' ');
+	execve(find_path(cmd[0], envp), cmd, envp);
 }
 
-void	parent_proc(int argc, char **argv, char **envp)
+void	parent_proc(char **argv, char **envp)
 {
-
+	wait(NULL);
+	char	**cmd;
+	cmd = ft_split(argv[3], ' ');
+	execve(find_path(cmd[0], envp), cmd, envp);
 }
 
 int		main(int argc, char **argv, char **envp)
 {
-	pid_t pid;
+	int pid;
 
 	pid = fork();
+	(void)argc;
 	if (pid == -1)
 		exit_error("fork");
 	else if (pid == 0)
-		child_proc(argc, argv, envp);
-	else if (pid == 1)
-		parent_proc(argc, argv, envp);
-	return (0);
+		child_proc(argv, envp);
+	else
+	{
+		parent_proc(argv, envp);
+	}
+	wait(&pid);
+	return (EXIT_SUCCESS);
 }
